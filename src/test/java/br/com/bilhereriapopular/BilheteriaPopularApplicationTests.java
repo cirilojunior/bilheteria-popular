492f78ea0e193875c1f67e590cf65b31b482748f
Cirilo Xavier <cirilolxjunior@gmail.com>
2026-02-28 23:51:06 -0300

    test: Correção de Testes

--- src/test/java/br/com/bilhereriapopular/BilheteriaPopularApplicationTests.java
+++ src/test/java/br/com/bilhereriapopular/BilheteriaPopularApplicationTests.java
@@ -1,13 +1,14 @@
-package com.example.demo;
+package br.com.bilhereriapopular;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BilheteriaPopularApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
